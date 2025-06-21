47346c2fd6ab8b5b84f354ff954809143c55b216
Shah Kamal Akonda <137825427+Shah-Kamal-Akonda@users.noreply.github.com>
2025-06-21 10:59:23 +0600

    change url to public url in product ,category and card slider page

--- frontend/src/app/components/CartSlider.tsx
+++ frontend/src/app/components/CartSlider.tsx
@@ -25,7 +25,7 @@ interface CartSliderProps {
   onOrderNow: () => void;
 }
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const CartSlider: React.FC<CartSliderProps> = ({
   cart,
