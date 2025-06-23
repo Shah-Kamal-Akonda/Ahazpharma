9d3cd09e45393609635c69847ea83857e834eb0f
Shah Kamal Akonda <137825427+Shah-Kamal-Akonda@users.noreply.github.com>
2025-06-23 19:39:36 +0600

    updated files layout , page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,13 +22,17 @@ export default function RootLayout({
       <body className={inter.className}>
         <Suspense
           fallback={
-            <div className="bg-white p-4 shadow-lg font-poppins">
+            <nav className="bg-white p-4 shadow-lg font-poppins">
               <div className="container mx-auto flex items-center justify-between">
                 <div className="w-[120px] h-[50px] bg-gray-200 rounded animate-pulse" />
                 <div className="w-full max-w-sm h-10 bg-gray-200 rounded-lg animate-pulse md:block hidden" />
-                <div className="w-12 h-12 rounded-full bg-gray-200 animate-pulse hidden md:flex" />
+                <div className="flex items-center space-x-3">
+                  <div className="w-12 h-12 rounded-full bg-gray-200 animate-pulse hidden md:block" />
+                  <div className="w-8 h-8 rounded-full bg-gray-200 animate-pulse md:hidden" />
+                  <div className="w-6 h-6 bg-gray-200 rounded animate-pulse md:hidden" />
+                </div>
               </div>
-            </div>
+            </nav>
           }
         >
           <Navbar />
